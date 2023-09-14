d9d81fd8f33f2a8d41c0234e02d3c51a42639743
HUDAAYOUB <huda3ya@gmail.com>
2023-09-14 18:21:54 +0300

    Q2

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+class Main {
+
+    public static void main (String[] args)
+    {
+        int number = 5;
+        if (number == 1) {
+            System.out.println("ONE");
+        }
+        else if (number == 2) {
+            System.out.println("TWO");
+        }
+
+        else if (number == 3) {
+            System.out.println("THREE");
+        }else if (number == 4) {
+            System.out.println("FOUR");
+        }else if (number == 5) {
+            System.out.println("FIVE");
+        }else if (number == 6) {
+            System.out.println("SIX");
+        }}}
